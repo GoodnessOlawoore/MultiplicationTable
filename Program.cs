cbd692d2677b215e5f5482002bd2df4e6f7ee35b
GoodnessOlawoore <olawooregoodness6@gmail.com>
2021-08-06 14:56:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Multiplication Table 1 - 12
+            Console.WriteLine("Multiplication Table 1 - 12:\n");
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 0; j <= 12; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
